7fba2f9f483ce99726e4bf71c7669f596cfbd92b
Mr-JM316 <75275830+Mr-JM316@users.noreply.github.com>
2021-11-27 13:04:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListScreen from "./src/screens/ListScreen";
+import ImageScreen from "./src/screens/ImageScreen";
+import CounterScreen from "./src/screens/CounterScreen";
+import TextScreen from "./src/screens/TextScreen";
+import PasswordScreen from "./src/screens/PasswordScreen";
+
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Image: ImageScreen,
+    Counter: CounterScreen,
+    Text: TextScreen,
+    Passw: PasswordScreen
+
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'ASOFT Enterprises',
+    },
+  }
+);
+
+export default createAppContainer(navigator);
